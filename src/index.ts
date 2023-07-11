7f88188bd290344d13a7f964d863f0cac15b4e5d
Andrey Gudin <aeon150791@gmail.com>
2023-07-11 19:54:53 +0400

    feat: change connection string

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,22 @@
-import mongoose from 'mongoose';
-import { PORT } from './constants';
+import mongoose from "mongoose";
+import { PORT } from "./constants";
 
-import * as serverService from './services/server.service';
-
+import * as serverService from "./services/server.service";
 
 (async () => {
   try {
-    await mongoose.connect('mongodb://mongo:jHCnIZJAsgHoSwmhZbUV@containers-us-west-11.railway.app:7669/pma');
+    await mongoose.connect(
+      "mongodb://mongo:vN9unVwuTF6wkhT25KPj@containers-us-west-136.railway.app:6130"
+    );
     serverService.server.listen(process.env.PORT || PORT, function () {
-      console.log('Сервер ожидает подключения...');
-    })
+      console.log("Сервер ожидает подключения...");
+    });
   } catch (error) {
     console.log(error);
   }
 })();
 
-
-
-process.on('SIGINT', async () => {
+process.on("SIGINT", async () => {
   await mongoose.disconnect();
   process.exit();
 });
